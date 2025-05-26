3a8f58772e3e71b60f844aac32d8e27a309319d7
Steven-LG09 <stevegiraldo224@gmail.com>
2025-05-26 14:04:32 -0400

    VimoCommit3

--- Frontend/src/app/pages/resume-main/resume-main.component.ts
+++ Frontend/src/app/pages/resume-main/resume-main.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-resume-main',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './resume-main.component.html',
   styleUrl: './resume-main.component.css'
 })
